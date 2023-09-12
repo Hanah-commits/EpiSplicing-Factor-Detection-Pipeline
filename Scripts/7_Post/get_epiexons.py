4ed70a3264e7f2eacdf7f1ff1a9905571f8f57c4
Hanah-commits <hanahbetsy25@gmailc.om>
2023-09-12 15:23:04 +0200

    added feature and score fields to bedfile

--- Scripts/7_Post/get_epiexons.py
+++ Scripts/7_Post/get_epiexons.py
@@ -7,9 +7,11 @@ def bedtools_input(hm, all_flanks):
     file = '0_Files/dPSI_Mval_epi_' + hm + '.csv'
     hm_flanks = pd.read_csv(file, delimiter='\t')
     epi_flanks = all_flanks[all_flanks['flanks'].isin(list(set(hm_flanks['flanks'].values)))]
+    epi_flanks['score'] = '.'
+    epi_flanks['feature'] = 'flanks'
 
     #bedtools input
-    epi_flanks[['seqid', 'start', 'stop', 'strand']].to_csv('0_Files/flanks_'+ hm + '.bed', sep='\t', index=False, header=False)
+    epi_flanks[['seqid', 'start', 'stop', 'feature', 'score', 'strand']].to_csv('0_Files/flanks_'+ hm + '.bed', sep='\t', index=False, header=False)
             
 
 def run_bedtools(hm):
