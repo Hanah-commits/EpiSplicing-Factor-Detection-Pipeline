eae622ee3c0ee6519e82e1a4742b882f10d55723
Hanah-commits <hanahbetsy25@gmailc.om>
2023-09-13 21:01:02 +0200

    process bedtools output

--- Scripts/7_Post/get_epiexons.py
+++ Scripts/7_Post/get_epiexons.py
@@ -21,9 +21,23 @@ def run_bedtools(hm):
 
 
 def post_bedtools(hm):
-    """
-    #TODO
-    """
+
+    exons = pd.read_csv('0_Files/epiexons_' + hm + '.bed', delimiter='\t', header=None)
+    # # drop feature, strand, score etc.
+    # exons.drop([3, 4, 5, 9, 10, 11], axis=1, inplace=True)
+    # # assign 0 to exons that have no flanks
+    # exons.replace([-1, '.'], [0, 0], inplace=True)
+    # exons = exons.set_axis(['seqid', 'exon_start', 'exon_stop', 'chr', 'flank_start', 'flank_stop'],
+    #                axis=1) #=True)
+    
+    # # keep exons that have flank annotation
+    # exons = exons[exons['flank_start'] !=0]
+
+    # #keep seq id and co-ordinates of exons
+    # exons = exons[['seqid', 'exon_start', 'exon_stop']]
+    # exons.drop_duplicates(inplace=True)
+
+    exons.to_csv('0_Files/epiexons_' + hm + '.bed', sep='\t', header=False, index=None)
 
 
 if __name__ == "__main__":
@@ -36,5 +50,6 @@ if __name__ == "__main__":
     flanks = pd.read_csv('0_Files/all_flanks.csv', delimiter='\t')
 
     for hm in hms[:1]:
-        bedtools_input(hm, flanks)
-        run_bedtools(hm)
+        # bedtools_input(hm, flanks)
+        # run_bedtools(hm)
+        post_bedtools(hm)
