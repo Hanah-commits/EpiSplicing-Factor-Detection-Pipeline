e03b48939abc8ebd12a6f220291e2df9a4bd03c2
Hanah-commits <57112634+Hanah-commits@users.noreply.github.com>
2023-11-20 11:11:41 +0100

    Create post-bedtools.py

---
+++ Scripts/1_RMATS/post-bedtools.py
@@ -0,0 +1,111 @@
+import pandas as pd
+import os
+import json
+
+exon_types = ['CS', 'AS']
+event_types = ['A3SS', 'A5SS', 'SE', 'MXE']
+flank_lens = [50, 100, 200]
+
+for exon_type in exon_types:
+        for type in event_types:
+                print(exon_type, type)
+
+                junctions = pd.read_csv(f'0_Files/{type}_exons_{exon_type}.csv', delimiter='\t')
+
+                with open('paths.json') as f:
+                        d = json.load(f)
+
+                fasta = d['Reference fasta']
+                ref_genome= fasta+".fai"
+
+                annot = []
+                for length in flank_lens:
+
+                        file = f'{type}_flanks{length}_{exon_type}.bed'
+
+                #         # adjust flank length to 200bp +- without exceeding chromosome bounds
+                        if length < 200:
+                        
+                                adjust_size = str(200 -length)
+
+                                # separate start,stop flank coords
+                                os.system("cut -f 1-3 0_Files/"+  file +" > 0_Files/coords.bed")
+
+                                # adjust flank boundaries
+                                os.system("bedtools slop -i 0_Files/coords.bed" + " -g " + ref_genome + " -b " + adjust_size + " > 0_Files/coords_adjusted.bed")
+
+                                # replace flank coords with adjusted coords
+                                os.system("awk 'FNR==NR{a[NR]=$2;next}{$2=a[FNR]}1' 0_Files/coords_adjusted.bed 0_Files/" + file + " > 0_Files/adjusted_flanks.bed")
+                                os.system("awk 'FNR==NR{a[NR]=$3;next}{$3=a[FNR]}1' 0_Files/coords_adjusted.bed 0_Files/adjusted_flanks.bed > 0_Files/adjusted.bed")
+                                os.system("sed 's/ /\t/g' 0_Files/adjusted.bed > 0_Files/"+file)
+
+                                # remove intermediate files
+                                os.system("rm 0_Files/coords*.bed")
+                                os.system("rm  0_Files/adjusted*.bed")
+
+
+                        flanks = pd.read_csv('0_Files/'+ file, delimiter='\t', header=None)
+
+                        # drop flanks that have no junction
+                        ##chrY    13359417        13360117        Exon    .       -       chrY    13359767        13359768        flank   .       - 
+                        ##chr10   100041843       100042543       Exon    .       -       .       -1      -1      .       -1      . 
+
+                        flanks = flanks[flanks[8] != -1]
+                        
+
+                        # merge the flanks df with the jns df
+                        flanks[12] = flanks[[1, 2]].apply(lambda row: '-'.join(row.values.astype(str)), axis=1)
+                        flanks.drop([3, 4, 5, 6, 8, 9, 10, 11], axis=1, inplace=True)
+                        flanks.columns = ['chr', 'start', 'stop', 'exon_coord0', 'flanks']
+
+                        junctions['index'] = junctions.index
+                        flank_jns = pd.merge(junctions, flanks, on=['exon_coord0', 'chr'])
+
+                        flank_jns.sort_values(['index'], inplace=True)
+                        flank_jns = flank_jns.reset_index(drop=True)
+                        del (flank_jns['index'])
+                        flank_jns['dpsi_'+str(length)] = pd.to_numeric(flank_jns['IncLevelDifference'])
+
+                        # # get all junctions that belong to each flank
+                        flank_jns_group = flank_jns.groupby(['flanks'])['dpsi_'+str(length)] \
+                                .apply(lambda val: ','.join(str(v) for v in val)).reset_index()
+                        annot.append(flank_jns_group)
+
+
+                for a in annot:
+                        a.set_index('flanks',inplace=True)
+
+                df = pd.concat(annot,axis=1,sort=False).reset_index()
+                df.dpsi_50.fillna(df.dpsi_100, inplace=True)
+                df.dpsi_50.fillna(df.dpsi_200, inplace=True)
+                flank_jns_group = df.drop(['dpsi_100', 'dpsi_200'], axis=1)
+                flank_jns_group.columns = ['flanks', 'IncLevelDifference']
+
+                # FILTER 3: if flank has 1+ junctions, keep junction with highest dPSI value
+                flank_jns_group['max_dPSI'] = flank_jns_group['IncLevelDifference'].str.split(',')\
+                        .apply(lambda x: min(map(float, x)))  # string -> list of strings -> list of floats -> max float
+
+                # # get the corresponding junction for each flank's max dPSI value
+                flank_jns_group = pd.merge(flank_jns_group[['flanks', 'max_dPSI']], flank_jns, on=['flanks'], how='inner')
+                flank_jns_group = flank_jns_group[flank_jns_group['IncLevelDifference'] == flank_jns_group['max_dPSI']]
+
+                # FILTER 4: If flank has 1+ junctions with same max dPSI value, keep one
+                flank_jns_group.drop_duplicates(subset='flanks', keep='first', inplace=True)
+
+                # # bookkeeping
+                del(flank_jns_group['max_dPSI'])
+                flank_jns_group = flank_jns_group[['GeneID', 'geneSymbol', 'chr', 'exon_coord0', 'IncLevelDifference',
+                        'FDR', 'flanks', 'start', 'stop', 'strand']]
+                
+
+                # # Get all filtered flanks
+                flank_jns_group.drop_duplicates().to_csv(f'0_Files/all_{type}_flanks_{exon_type}.csv', sep='\t', index=False)
+
+                # # Get new annotation file with flanks (to annotate MANorm peaks)
+                flank_jns_group[['chr', 'start', 'stop']].drop_duplicates().to_csv(f'0_Files/filtered_{type}_flanks_{exon_type}.bed', index=False, sep='\t', header=False)
+
+                # # Get dPSI values of filtered flanks
+                flank_jns_group[['chr', 'flanks', 'geneSymbol', 'IncLevelDifference']].drop_duplicates().to_csv(f'0_Files/Filtered_dPSI_{type}_{exon_type}.csv', index=False, sep='\t')
+
+
+
