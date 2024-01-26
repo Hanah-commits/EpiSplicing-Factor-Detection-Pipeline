7644bbb7400544c87ac2954b38e82d0298fa5cb7
Hanah-commits <hanahbetsy25@gmailc.om>
2024-01-26 20:09:26 +0100

    DHM annotation of DEXSEQ exon flanks

---
+++ Scripts/2_MANorm/annotate_MANorm_DEXSEQ.py
@@ -0,0 +1,19 @@
+import os
+import sys
+import json
+
+with open('paths.json') as f:
+    d = json.load(f)
+
+tissue1 = d["tissue1"]
+tissue2 = d["tissue2"]
+hms = d["Histone modifications"]
+
+prefix = sys.argv[1]+ 'MANorm/'
+
+for hm in hms:
+    
+    input = prefix + hm + '_' + tissue1 + '_peak_vs_' + hm + '_' + tissue2 +  '_peak_all_MAvalues.xls'
+    output = prefix + hm + '_dexseq_flanks.bed'
+
+    os.system('bedtools intersect -loj -a 0_Files/dexseq_flanks200.bed -b ' + input + ' | sort | uniq > ' + output)
