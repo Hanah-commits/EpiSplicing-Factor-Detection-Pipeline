3ee0589834dc726fef651c5c816b7bc8f55aa17d
Hanah-commits <hanahbetsy25@gmailc.om>
2024-03-11 15:29:58 +0100

    run dexseq with featurecounts count matrix

--- Scripts/1.3_DEXSEQ/runDEXSEQ.py
+++ Scripts/1.3_DEXSEQ/runDEXSEQ.py
@@ -18,10 +18,10 @@ odir = sys.argv[1] + 'DEXSEQ/'
 Path(odir).mkdir(parents=True, exist_ok=True)
 
 # prepare flattened annotation file
-os.system(f"python {dexseq_scripts}python_scripts/dexseq_prepare_annotation.py {ref} {odir}DEXSEQ_reference.gtf")
+os.system(f"python {dexseq_scripts}python_scripts/dexseq_prepare_annotation_subread.py -f {odir}DEXSEQ_reference.gtf {ref} {odir}DEXSEQ_reference.gff")
 
 # count exons
-os.system(f'bash HelperFunction/count_all.sh {tissue1} {tissue2} {bam_files}')
+os.system(f'bash HelperFunctions/count_all.sh {tissue1} {tissue2} {bam_files}')
 
 # rundexseq
-os.system(f'Rscript run_dexseq {bam_files} {tissue1} {tissue2}')
+os.system(f'Rscript 1.3_DEXSEQ/run_dexseq.R {bam_files} {tissue1} {tissue2}')
