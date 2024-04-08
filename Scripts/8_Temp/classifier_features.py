d0ba86270a9e38e69ee99c300685b1f0155159bc
Hanah-commits <hanahbetsy25@gmailc.om>
2024-04-08 13:27:31 +0200

    keep HM info in feature matrix

---
+++ Scripts/8_Temp/classifier_features.py
@@ -0,0 +1,32 @@
+import pandas as pd
+import json
+
+with open('paths.json') as f:
+    data = json.load(f)
+
+hms = data["Histone modifications"]
+
+prefix = '0_Files/Post-processing'
+unscaled = ['features_epi.csv', 'features_nonepi.csv', 'all_features.csv']
+
+
+epi_features = pd.read_csv('0_Files/Post-processing/features_epi.csv', delimiter='\t')
+nonepi_features = pd.read_csv('0_Files/Post-processing/features_nonepi.csv', delimiter='\t')
+
+# keep nonepi flanks for hms available in current study
+nonepi_features = nonepi_features[nonepi_features['type'].apply(lambda x: any(item in hms for item in x.split(',')))]
+nonepi_features.to_csv('0_Files/Post-processing/features_nonepi.csv', sep='\t', index=False)
+
+epi_features['label'] = 'epigene'
+nonepi_features['label'] = 'non-epigene'
+
+all_features = pd.concat([epi_features, nonepi_features], axis=0)
+
+all_features.drop(['chr', 'exon_start', 'exon_end', 'feature', 'score', 'strand', 'gene_name'], axis=1, inplace=True)
+
+
+col = all_features.pop("label")
+all_features.insert(0, col.name, col)
+
+all_features.to_csv('0_Files/Post-processing/features_all.csv', sep='\t', index=False)
+
