44d62f3231891ca993404f1f68a7d573398b839b
Hanah-commits <hanahbetsy25@gmailc.om>
2025-04-18 18:08:28 +0200

    prep rbpmap input, process output, prep feature matrix

--- Scripts/4_RBPMap/post-rbp.py
+++ Scripts/4_RBPMap/post-rbp.py
@@ -1,154 +1,179 @@
-import csv
-import pandas as pd
-import numpy as np
-import os
-
-def adjust_pvalue(df, col):
-
-    # get indices of null values
-    na_idx = df[df[col].isnull()].index.tolist()
-
-    # adjust non-null p values
-    pvals = df[col].values.tolist()
-    pvals = [x for x in pvals if str(x) != 'nan']
-    adj_pval = p_adjust_bh(pvals).tolist()
-
-    # insert null at original indices
-    for idx in na_idx:
-        adj_pval.insert(idx, None)
-
-    # adjusted p values as new df
-    df.loc[:, 'adj_pval'] = adj_pval
-    return df
+import pandas as pd, numpy as np
+import re
 
 
-def p_adjust_bh(p):
-    ## multiple hypothesis testing
-    p = np.asfarray(p)
-    by_descend = p.argsort()[::-1]
-    by_orig = by_descend.argsort()
-    steps = float(len(p)) / np.arange(len(p), 0, -1)
-    q = np.minimum(1, np.minimum.accumulate(steps * p[by_descend]))
-    return q[by_orig]
+def post_rbp(rbp_num):
+  
+
+    for type in ['epi', 'nonepi']:
+        opdir = '0_Files/Post-processing'
+                    
+        rbps = open(f'../RBPmap_{str(rbp_num)}/rbps.txt', 'r') #'HelperFunctions/rbps_{rbp_num}.txt'
+        proteins = list(set([rbp for rbp in rbps.read().split('\n') if len(rbp)]))
+
+        # # proteins = ['RBM15'] #if single rbp
+
+        filename=f'../RBPmap_{str(rbp_num)}/results_rbp_input_{type}1.csv/All_Predictions.txt'
+
+        parsed_data = []
+
+        # open and read the input file
+        with open(filename, 'r') as file:
+            current_region = None
+            current_strand = None
+            current_protein = None
+            region_data = {protein: [] for protein in proteins}  # (zscore, p_value)
+
+            for line in file:
+                line = line.strip()
+
+                # get genomic regions and strands
+                if re.match(r'^chr', line):
+                    if current_region:
+                        # process region data before appending
+                        row_data = []
+                        for protein in proteins:
+                            z_p_tuples = region_data[protein]
+
+                            if z_p_tuples:
+                                # separate Z-scores and p-values
+                                zscores, p_values = zip(*z_p_tuples)
+                                # select max Z-score 
+                                selected_zscore = np.max(np.array(zscores, dtype=float)) 
+                                row_data.append(selected_zscore)
+
+                            else:
+                                row_data.append(0.0)
+
+                        # append the region data to parsed_data
+                        parsed_data.append(current_region + [current_strand] + row_data)
+
+                    # split line to extract chromosome, start, end, and strand information
+                    region_parts = re.split(r'[:-]', line)
+                    current_region = [region_parts[0], int(region_parts[1]), int(region_parts[2])]
+                    current_strand = region_parts[-1]
+
+                    # Reset region data for new region
+                    region_data = {protein: [] for protein in proteins}
+
+                # get protein blocks
+                elif line.startswith('Protein:'):
+                    if rbp_num == 47:
+                        current_protein = line.split('_')[1]
+                    else:
+                        current_protein = line.split('Protein: ')[1].split('(Hs/Mm)')[0]
+
+                # skip header lines
+                elif line.startswith('Sequence Position') or line.startswith('Z-score'):
+                    continue
+
+                # extract Z-scores and p-values
+                elif line and current_protein in proteins:
+                    line_parts = line.split()
+                    if len(line_parts) >= 6:  # check there are enough elements
+                        try:
+                            zscore = str(line_parts[-2]) # save most impt binding score
+                            p_value = float(line_parts[-1])
+                            region_data[current_protein].append((zscore, p_value))
+                        except ValueError:
+                            continue
+
+            # append the last region data
+            if current_region:
+                row_data = []
+                for protein in proteins:
+                    z_p_tuples = region_data[protein]
+
+                    if z_p_tuples:
+                        zscores, p_values = zip(*z_p_tuples)
+                        # select max Z-score 
+                        selected_zscore = np.max(np.array(zscores, dtype=float)) 
+                    else:
+                        row_data.append(0.0)
+
+                parsed_data.append(current_region + [current_strand] + row_data)
 
 
-def process_results(result_dirs, type):
-    proteins = ['BRUNOL4(Hs/Mm)', 'BRUNOL5(Hs/Mm)', 'BRUNOL6(Hs/Mm)', 'DAZAP1(Hs/Mm)', 'ESRP2(Hs/Mm)', 'FMR1(Hs/Mm)', 'FUS(Hs/Mm)', 'FXR1(Hs/Mm)', 'FXR2(Hs/Mm)', 'HNRNPA1(Hs/Mm)', 'HNRNPA1L2(Hs/Mm)', 'HNRNPA2B1(Hs/Mm)', 'HNRNPC(Hs/Mm)', 'HNRNPF(Hs/Mm)', 'HNRNPH1(Hs/Mm)', 'HNRNPH2(Hs/Mm)', 'HNRNPK(Hs/Mm)', 'HNRNPL(Hs/Mm)', 'HNRNPM(Hs/Mm)', 'HNRNPU(Hs/Mm)', 'HuR(Hs/Mm)', 'KHDRBS1(Hs/Mm)', 'KHDRBS2(Hs/Mm)', 'KHDRBS3(Hs/Mm)', 'MBNL1(Hs/Mm)', 'PABPC1(Hs/Mm)', 'PABPN1(Hs/Mm)', 'PCBP1(Hs/Mm)', 'PCBP2(Hs/Mm)', 'PTB3(Hs/Mm)', 'QKI(Hs/Mm)', 'RALY(Hs/Mm)', 'RBFOX1(Hs/Mm)', 'RBM24(Hs/Mm)', 'RBM28(Hs/Mm)', 'RBM3(Hs/Mm)', 'RBM4(Hs/Mm)', 'RBM42(Hs/Mm)', 'RBM5(Hs/Mm)', 'RBM8A(Hs/Mm)', 'SART3(Hs/Mm)', 'SFPQ(Hs/Mm)', 'SNRNP70(Hs/Mm)', 'SNRPA(Hs/Mm)', 'SRSF1(Hs/Mm)', 'SRSF10(Hs/Mm)', 'SRSF2(Hs/Mm)', 'SRSF7(Hs/Mm)', 'SRSF9(Hs/Mm)', 'TARDBP(Hs/Mm)', 'TIA1(Hs/Mm)', 'U2AF2(Hs/Mm)', 'YBX1(Hs/Mm)', 'ZC3H10(Hs/Mm)', 'ZCRB1(Hs/Mm)', 'ZNF638(Hs/Mm)']
-    dfs_collection = []
+        # # Create DataFrame from parsed data
+        df = pd.DataFrame(parsed_data, columns=['chr', 'flank_start', 'flank_end', 'strand'] + proteins)
 
-    zscore_collection = []  # holds 'zscore_list(s)' of all flanks
-    pval_collection = []
-
-    # STEP 1: Read RBPmap results into a dataframe
-    for current_dir in result_dirs:
-
-        file = os.path.join(current_dir, 'Predictions.txt')
-
-        # temp files
-        subdir = file.split('Predictions.txt')[0]
-        new_file = os.path.join(subdir, 'Predictions_new.txt')
-
-        # list of proteins with a putative binding site in the current flank
-        proteins_file = os.path.join(subdir, 'proteins.txt')
-
-        try:
-
-            os.system('grep -vwE "(Protein:)" ' + file + ' > ' + new_file)  # removing lines with protein names
-            os.system('sed -i "1,8d" ' + new_file)
-            os.system(
-                'grep -oP "(?<=Protein: ).*" ' + file + ' > ' + proteins_file)  # "Protein: BRUNOL4" -> BRUNOL4
-
-            result_df = pd.read_fwf(new_file, skip_blank_lines=True)
-            result_df = result_df[['Genomic Coordinate', 'Motif', 'Z-score', 'P-value']]
-
-            # STEP 2: Mark each Z score with its corresponding protein name
-
-            # indices of first and last occurrences of each protein's Zscores
-            idx = result_df.index[result_df['Motif'] == 'Motif'].tolist()
-            idx.insert(0, -1)
-            idx.append(len(result_df))
-
-            # list of proteins with binding sites in current flank
-            with open(proteins_file) as f:
-                binding_proteins = f.read().splitlines()
-
-            # construct column with appropriate num repeats of proteins
-            protein_col = []
-            i = 1
-            for p in binding_proteins:
-                if i <= len(binding_proteins):
-                    for j in range(idx[i] - idx[i - 1] - 1):
-                        protein_col.append(p)
-
-                    i += 1
-                else:
-                    break
-
-            # remove rows with text
-            result_df = result_df[result_df.Motif != 'Motif']
-            result_df.reset_index(drop=True, inplace=True)
-
-            # add protein column
-            result_df['protein'] = protein_col
-            dfs_collection.append(result_df)
-
-            # STEP 3: get largest Z score of each protein for the current flank
-            zscore_list = []
-            pval_list = []
-            for p in proteins:
-                protein_df = result_df.loc[result_df['protein'] == p].copy()
-                if protein_df.empty:  # protein has no binding site in flank
-                    zscore_list.append(0)
-                    pval_list.append(None)
-                else:  # assign z score based on significant adj p value
-                    protein_df.loc[:, 'P-value'] = pd.to_numeric(protein_df['P-value'])
-                    protein_df = adjust_pvalue(protein_df, 'P-value')
-                    protein_df = protein_df[protein_df['adj_pval'] <= 0.05]
-                    minidx = protein_df['P-value'].idxmin()
-                    significant_z = protein_df.loc[minidx]['Z-score']
-                    significant_p = protein_df.loc[minidx]['P-value']
-                    zscore_list.append(significant_z)
-                    pval_list.append(significant_p)
-
-            # no protein has binding site in flanks
-            if zscore_list.count(0) == len(zscore_list):
-                zscore_list = [None] * len(zscore_list)
-
-            zscore_collection.append(zscore_list)
-            pval_collection.append(pval_list)
-
-            # delete temp files
-            os.remove(proteins_file)
-            os.remove(new_file)
-
-        except:
-            pass
-
-    col_names = [p.split('(Hs/Mm)')[0] for p in proteins]
-    zscore_collection.insert(0, col_names)
-    pval_collection.insert(0,  col_names)
-
-    with open("0_Files/FilteredZscores_" + type + ".csv", "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(zscore_collection)
-
-    with open("0_Files/FilteredPvalues_" + type + ".csv", "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(pval_collection)
+        # Display the resulting DataFrame
+        # df[proteins].to_csv(f"{opdir}/FilteredZscores_{type}.csv", sep=',', index=None)
+        df.to_csv(f"{opdir}/FilteredZscores_{type}.csv", sep=',', index=None)
 
 
+def feature_matrix_1():
+
+    exons_files = ['0_Files/Post-processing/epi_flanks.bed', '0_Files/Post-processing/nonepi_flanks.bed']
+    Zscore_files = ['0_Files/Post-processing/FilteredZscores_epi.csv', '0_Files/Post-processing/FilteredZscores_nonepi.csv']
+
+    for i in range(2):
+        exons = pd.read_csv(exons_files[i], delimiter='\t', header=None)
+        exons.columns = ['chr', 'exon_start', 'exon_end', 'feature', 'score', 'strand', 'gene_name', 'type']
+
+        rbp = pd.read_csv(Zscore_files[i], delimiter=',')
+
+        features = pd.concat([exons, rbp], axis=1)
+
+        # double-check rbpmap results parsing
+        if len(features[features.exon_start != features.flank_start]) == 0 and len(features[features.exon_end != features.flank_end]) == 0: # check 
+            features = features.drop(columns=['flank_start', 'flank_end'])
+            features = features.loc[:, ~features.columns.duplicated(keep='first')] # keep first occurence of columns: chr, strand
+        else:
+            raise ValueError
+        
+        name = ''
+        if i == 0:
+            name = 'epi'
+        else:
+            name = 'nonepi'
+
+        features = features.loc[:, ~features.columns.duplicated()]
+        features.to_csv('0_Files/Post-processing/features_' + name + '.csv', sep='\t', index=False)
+
+
+def feature_matix_2(rbp_num):
+    epi_features = pd.read_csv('0_Files/Post-processing/features_epi.csv', delimiter='\t')
+    nonepi_features = pd.read_csv('0_Files/Post-processing/features_nonepi.csv', delimiter='\t')
+
+    # keep nonepi flanks for hms available in current study
+    nonepi_features = nonepi_features[nonepi_features['type'].apply(lambda x: any(item in ['H3K27ac', 'H3K27me3', 'H3K9me3', 'H3K4me3', 'H3K36me3'] for item in x.split(',')))]
+    # nonepi_features.to_csv('0_Files/Post-processing/features_nonepi.csv', sep='\t', index=False)
+
+    epi_features['label'] = 'epigene'
+    nonepi_features['label'] = 'non-epigene'
+
+    all_features = pd.concat([epi_features, nonepi_features], axis=0)
+
+    # remove genes with both labels
+    common_genes = list(set(epi_features.gene_name.values.tolist()) & set(nonepi_features.gene_name.values.tolist()))
+    all_features = all_features[~((all_features.gene_name.isin(common_genes)) & (all_features.label == 'non-epigene'))]
+    all_features = all_features[~((all_features.gene_name.isin(common_genes)) & (all_features.label == 'epigene'))]
+
+    for hm in ['H3K27ac', 'H3K27me3', 'H3K9me3', 'H3K4me3', 'H3K36me3']:
+        print('\n',hm)
+        temp_features = all_features[all_features['type'].apply(lambda x: hm in x.split(','))]
+        print('Epigenes:', len(set(temp_features[temp_features.label=='epigene'].gene_name.values)), 'Flanks:', len(temp_features[temp_features.label=='epigene']))
+        print('Nonepigenes:', len(set(temp_features[temp_features.label=='non-epigene'].gene_name.values)), 'Flanks:', len(temp_features[temp_features.label=='non-epigene']))
+
+    all_features.drop(['chr', 'exon_start', 'exon_end', 'feature', 'score', 'strand', 'gene_name'], axis=1, inplace=True)
+
+
+    col = all_features.pop("label")
+    all_features.insert(0, col.name, col)
+
+    all_features.to_csv(f'0_Files/Post-processing/features_all_{rbp_num}.csv', sep='\t', index=False)
 
 
 
 if __name__ == "__main__":
-    path = '../RBPmap'
-    results_dirs = [x[0] for x in os.walk(path)]
-    epi_dirs = [p for p in results_dirs if 'resultsrbp_input_epi' in p and 'sequence' in p]
-    nonepi_dirs = [p for p in results_dirs if 'resultsrbp_input_nonepi' in p and 'sequence' in p]
-
-    process_results(epi_dirs, 'epi')
-    process_results(nonepi_dirs, 'nonepi')
-
-    print('processed')
-
-
+    
+    # prep feature zscore matrix -132 RBPS
+    post_rbp(132)
+    feature_matrix_1()
+    feature_matix_2(132)
+    
+    # prep feature zscore matrix - 47 RBPS
+    post_rbp(47)
+    feature_matrix_1()
+    feature_matix_2(47)
--- Scripts/4_RBPMap/pre-rbp.py
+++ Scripts/4_RBPMap/pre-rbp.py
@@ -1,48 +1,43 @@
 import pandas as pd
 import os
 
-flanks = pd.read_csv('0_Files/all_flanks.csv', delimiter='\t')
-epigene_flanks = pd.read_csv('0_Files/dPSI_Mval_epi.csv', delimiter='\t')
-nonepigene_flanks = pd.read_csv('0_Files/dPSI_Mval_nonepi.csv', delimiter='\t')
+def exon_flanks():
 
-epi_flanks = flanks[flanks['flanks'].isin(list(set(epigene_flanks['flanks'].values)))]
-nonepi_flanks = flanks[flanks['flanks'].isin(list(set(nonepigene_flanks['flanks'].values)))]
+        input_files = []
+        op_dir = "0_Files/Post-processing"
 
-input_files = []
+        for name in ['epi', 'nonepi']:
 
-# Get input sequences for RBPmap
-n = 0
-i = 1
-while n < len(epi_flanks):
-        name = '../RBPmap/rbp_input_epi'+str(i)+'.csv'
-        input_files.append(name)
-        if n+5000 <= len(epi_flanks):
-                epi_flanks[['seqid', 'flanks', 'strand']].iloc[n:n+5000].to_csv(name, index=False, sep=':', header=False)
-                n += 5000
-        else:
-                epi_flanks[['seqid', 'flanks', 'strand']].iloc[n:len(epi_flanks)+1].to_csv(name, index=False, sep=':', header=False)
-                break
-        i += 1
+                flanks = pd.read_csv(f'{op_dir}/{name}_flanks.bed', delimiter='\t', header=None)
+                flanks.columns = ['chr', 'flank_start', 'flank_end', 'feature', 'score', 'strand', 'gene_name', 'type']
+                flanks['flanks'] = flanks[['flank_start', 'flank_end']].apply(lambda row: '-'.join(row.values.astype(str)), axis=1)
+                
+                # Get input sequences for RBPmap
+                n = 0
+                i = 1
+                while n < len(flanks):
+                        file_name = f'../RBPmap/rbp_input_{name}{i}.csv'
+                        input_files.append(file_name)
+                        if n+5000 <= len(flanks):
+                                flanks[['chr', 'flanks', 'strand']].iloc[n:n+5000].to_csv(file_name, index=False, sep=':', header=False)
+                                n += 5000
+                        else:
+                                flanks[['chr', 'flanks', 'strand']].iloc[n:len(flanks)+1].to_csv(file_name, index=False, sep=':', header=False)
+                                break
+                        i += 1
 
-# Get input sequences for RBPmap
-n = 0
-i = 1
-while n < len(nonepi_flanks):
-        name = '../RBPmap/rbp_input_nonepi'+str(i)+'.csv'
-        input_files.append(name)
-        if n+5000 <= len(nonepi_flanks):
-                nonepi_flanks[['seqid', 'flanks', 'strand']].iloc[n:n+5000].to_csv(name, index=False, sep=':', header=False)
-                n += 5000
-        else:
-                nonepi_flanks[['seqid', 'flanks', 'strand']].iloc[n:len(nonepi_flanks)+1].to_csv(name, index=False, sep=':', header=False)
-                break
-        i += 1
 
-# # get flanks for feature matrix preparation step
-epi_flanks[['gene_id', 'flanks']].to_csv('0_Files/query_flanks_epi.csv', sep='\t', index=False)
-nonepi_flanks[['gene_id', 'flanks']].to_csv('0_Files/query_flanks_nonepi.csv', sep='\t', index=False)
+                # # get flanks for feature matrix preparation step
+                flanks[['gene_name', 'flanks']].to_csv(f'{op_dir}/query_flanks_{name}.csv', sep='\t', index=False)
 
-input_files = [os.getcwd() + '/' + file for file in input_files]
-with open('../RBPmap/input.txt', 'w') as f:
-    for item in input_files:
-        f.write("%s\n" % item)
+        # write RBPmap input
+        input_files = [os.getcwd() + '/' + file for file in input_files]
+        with open('../RBPmap/input.txt', 'w') as f:
+                for item in input_files:
+                        f.write("%s\n" % item)
+
+
+
+if __name__ == "__main__":
+
+        exon_flanks()
